538d52135627e185872fc9a385e236fbae29e1f5
Jose Hermosilla <jose.hermosillaasach.cl>
2021-07-23 20:52:31 -0400

    sese termina el ejercicio 2

--- 02countries.sql
+++ 02countries.sql
@@ -62,10 +62,34 @@ SELECT
   government_form,
   capital,
   life_expectancy
-  
 FROM
   countries
 WHERE
   government_form = 'Constitutional Monarchy'
   AND capital > 200
   AND life_expectancy > 75;
+#7. ¿Qué consulta harías para obtener todas las ciudades de Argentina dentro del distrito de Buenos Aires y tener una población superior a 500,000?
+  #La consulta debe devolver el nombre del país, el nombre de la ciudad, el distrito y la población. (2)
+SELECT
+  countries.name as country_name,
+  cities.name as city_name,
+  district,
+  cities.population as city_population
+FROM
+  countries
+  JOIN cities ON countries.id = cities.country_id
+WHERE
+  district = 'Buenos Aires'
+  and cities.population > 500000
+ORDER BY
+  cities.population DESC;
+
+#¿Qué consulta harías para resumir el número de países en cada región?
+  #La consulta debe mostrar el nombre de la región y el número de países. Además, la consulta debe organizar el resultado por el número de países en orden descendente. (2)
+SELECT
+  region as region_name,
+  COUNT(*) as number_of_countries
+FROM
+  countries
+GROUP BY
+  region ORDER BY COUNT(*) DESC;
